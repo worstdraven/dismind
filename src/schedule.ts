ef31f9ec1c0fc4fc49044b50face80a2cf9106e9
worstdraven <kadennolan9@gmail.com>
2024-11-11 23:38:18 +0900

    feat: add webhook connection

--- src/schedule.ts
+++ src/schedule.ts
@@ -73,3 +73,35 @@ function getJapaneseDay(date: Date): string {
   const japaneseDays = ["月", "火", "水", "木", "金", "土", "日"];
   return japaneseDays[dayNum - 1] + "曜日";
 }
+
+function discordHook() {
+  const STATUS = PropertiesService.getScriptProperties().getProperty("STATUS");
+  const WEBHOOK_URL = PropertiesService.getScriptProperties().getProperty(`WEBHOOK_URL_${STATUS}`);
+
+  const embedTitle =
+    Utilities.formatDate(getDate(1), "Asia/Tokyo", "M月d日") +
+    "（" +
+    getJapaneseDay(getDate(1)) +
+    "）の予定";
+
+  const message = {
+    username: "B.F. Sword",
+    avatar_url:
+      "https://static.wikia.nocookie.net/leagueoflegends/images/a/ab/B._F._Sword_item.png/revision/latest?cb=20201029192225",
+    embeds: [
+      {
+        title: embedTitle,
+        url: "https://calendar.google.com/calendar",
+        type: "rich",
+        description: createScheduleEmbed(),
+      },
+    ],
+  };
+
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: "post",
+    headers: { "Content-type": "application/json" },
+    payload: JSON.stringify(message),
+  };
+  UrlFetchApp.fetch(WEBHOOK_URL, options);
+}
